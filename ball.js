a00ba3037016f5602a3166a1a24465ae993bc056
Jegan_Karthi <125629033+jegankarthi@users.noreply.github.com>
2023-05-10 09:40:23 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,42 @@
+let velocityX= 5;
+let velocityY= 5;
+let positionX = 0;
+let positionY = 0;
+let positionxmin = 0;
+let positionxmax = 250;
+let positionymin = 0;
+let positionymax = 250;
+let time = 15;
+let reverse_X = true;
+let reverse_Y= true;
+let ball= document.getElementById('ball');
+
+function moveBall() {
+  if(reverse_X){
+    positionX = positionX + velocityX;
+    ball.style.top = positionY + 'px';
+    ball.style.right = positionX + 'px';
+  }else{
+    positionX = positionX - velocityX;
+    ball.style.right = positionY + 'px';
+    ball.style.top = positionX + 'px';
+ }
+ if(reverse_Y){
+  positionY = positionY + velocityY;
+  ball.style.left = positionX + 'px';
+  ball.style.bottom = positionY + 'px';
+}else{
+  positionY = positionY - velocityY;
+  ball.style.top = positionX - 'px';  
+  ball.style.left = positionX + 'px';
+}
+
+ if(positionX > positionxmax || positionX === positionxmin){
+    reverse_X = !reverse_X;
+  }
+
+  if(positionY > positionymax || positionY === positionymin){
+    reverse_Y = !reverse_Y;
+  }
+}
+  setInterval(moveBall, time);
